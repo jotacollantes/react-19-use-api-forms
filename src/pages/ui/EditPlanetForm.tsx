359e458a32efe6ba75752b9329ae28c537327a49
Fernando Herrera <fernando.herrera85@gmail.com>
2024-12-11 09:38:35 -0500

    Add Neptuno to planets data, create SubmitButton component, and update EditPlanetForm

--- src/pages/ui/EditPlanetForm.tsx
+++ src/pages/ui/EditPlanetForm.tsx
@@ -1,6 +1,7 @@
 import { useActionState } from 'react';
 import { Planet } from '../../interfaces/planet.interface';
 import { createPlanetActionForm } from '../../actions/create-planet.action';
+import { SubmitButton } from './SubmitButton';
 
 interface Props {
   onAddPlanet: (planet: Planet) => void;
@@ -17,7 +18,7 @@ export const EditPlanetForm = ({ onAddPlanet }: Props) => {
 
   return (
     <form className="mb-4 flex flex-col md:flex-row" action={formAction}>
-      <h1>{isPending ? 'Pending' : 'No pending'}</h1>
+      {/* <h1>{isPending ? 'Pending' : 'No pending'}</h1> */}
 
       <input
         type="text"
@@ -40,13 +41,15 @@ export const EditPlanetForm = ({ onAddPlanet }: Props) => {
         name="distanceFromSun"
         required
       />
-      <button
+
+      <SubmitButton />
+      {/* <button
         type="submit"
         className="bg-blue-500 disabled:bg-gray-500 text-white p-2 rounded flex-1 sm:flex-none"
         disabled={isPending}
       >
         Agregar planeta
-      </button>
+      </button> */}
     </form>
   );
 };
